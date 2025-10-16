bc7fd9ccf1725db30af5b6d732441cf32aedee33
Leandro Marques <leandro.hdsl@gmail.com>
2025-10-16 11:49:52 -0300

    Atualizado projeto com as depdendecias atuais e funcionais

--- app/src/main/java/com/example/leandromarques/chatbotdoid/BotActivity.java
+++ app/src/main/java/com/example/leandromarques/chatbotdoid/BotActivity.java
@@ -4,10 +4,7 @@ import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,13 +12,16 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.leandromarques.chatbotdoid.Adapter.ChatMessageAdapter;
 import com.example.leandromarques.chatbotdoid.Pojo.ChatMessage;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.alicebot.ab.AIMLProcessor;
 import org.alicebot.ab.Bot;
